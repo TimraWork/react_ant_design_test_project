93b2c8131617a50b718aa0895a8d86d0227c9943
Timra <59473768+TimraWork@users.noreply.github.com>
2021-11-19 09:59:42 +0500

    feat: 🎸 disable submit if empty inputs in ModalAddCards

--- src/components/ModalAddCard/ModalAddCard.tsx
+++ src/components/ModalAddCard/ModalAddCard.tsx
@@ -1,5 +1,5 @@
-import { Input, Modal, Space } from 'antd';
-import React from 'react';
+import { Input, Modal, Space, Button } from 'antd';
+import React, {useEffect, useState} from 'react';
 import useInput from '../../customHooks/useInput';
 
 export type IModalAddCardProps = {
@@ -12,13 +12,34 @@ const ModalAddCard: React.FC<IModalAddCardProps> = ({ isModalVisible, handleOk, 
     const city = useInput("");
     const population = useInput("");
 
+    const [isCreateButtonDisable, setIsCreateButtonDisable] = useState(true);
+
+    useEffect(() => {
+        if(city.error || population.error) {
+            setIsCreateButtonDisable(true);
+        }
+        else if(!(city.error && population.error)) {
+            setIsCreateButtonDisable(false);
+        }
+    }, [city, population]);
+
     const onModalClose = () => {
        city.reset();
        population.reset();
     }
 
     return (
-        <Modal afterClose={onModalClose} title="Создать карточку" visible={isModalVisible} onOk={() => handleOk(city.value, population.value)} onCancel={handleCancel} cancelText="Отмена" okText="Создать">
+        <Modal
+            afterClose={onModalClose}
+            title="Создать карточку"
+            visible={isModalVisible}
+            destroyOnClose={true}
+            onCancel={handleCancel}
+            footer={[
+                <Button key="reset" type="default" onClick={handleCancel} > Отмена </Button>,
+                <Button key="submit" type="primary" onClick={() => handleOk(city.value, population.value)} disabled={isCreateButtonDisable}> Создать </Button>,
+            ]}
+        >
             <Space direction="vertical" style={{ width: '100%' }}>
                 <Input autoFocus={true} placeholder="Введите название города" value={city.value} onChange={city.onChange} onBlur={city.onBlur} />
                 {city.error && <div className="error">{city.error}</div>}
